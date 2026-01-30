b8f1be1b4a922fdc106a153fd627b3a5a937d122
Ayushi shahi <ayushishahi14072004@gmail.com>
2026-01-31 02:26:06 +0530

    Frontend url changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,10 +52,11 @@ app = FastAPI(
 # -----------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://eventpulse-frontend.onrender.com",
-        "http://localhost:3000",
-    ],
+    # allow_origins=[
+    #     "https://eventpulse-frontend.onrender.com",
+    #     "http://localhost:3000",
+    # ],
+    allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=[
