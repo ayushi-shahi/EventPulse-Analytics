989ee50eacfdf945b98909e534fb692b7cce4c88
Ayushi shahi <ayushishahi14072004@gmail.com>
2026-01-31 01:01:38 +0530

    Deploy changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,11 +52,15 @@ app = FastAPI(
 # -----------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://eventpulse.onrender.com",
+        "http://localhost:3000",  
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # -----------------------
 # Request Logging Middleware
