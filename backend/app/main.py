490a79c98d521e9272a73feaf6b634fc93e47efc
Ayushi shahi <ayushishahi14072004@gmail.com>
2026-01-31 02:03:36 +0530

    Frontend url changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -54,11 +54,14 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://eventpulse-frontend.onrender.com",
-        "http://localhost:3000",  
     ],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=[
+        "Authorization",
+        "Content-Type",
+        "Accept",
+    ],
 )
 
 
